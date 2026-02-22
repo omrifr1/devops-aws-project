2b1988147be44c1cdd1e83c13c0094564a7c8d0f
omrifr1 <omri.farnk@gmail.com>
2026-02-22 18:39:43 +0200

    Infrastructure: Separate variables and add EC2 Connect rule

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "my_ip" {
+  type        = string
+  description = "My Home IP address"
+}
+
+variable "region" {
+  default = "eu-north-1"
+}
+
+variable "project_name" {
+  default = "sprint-feb-2026"
+}
