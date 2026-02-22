18aa6ce55afe5c887ac4bfb26e1236f078d73b84
omrifr1 <omri.farnk@gmail.com>
2026-02-22 18:27:47 +0200

    Refactor: Integrated variables and refined security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,8 @@
 # העתק את זה ל-main.tf שנפתח לך:
-
+variable "my_ip" {
+  default = "77.137.84.125/32"
+  description = "My Home IP address"
+}
 terraform {
   required_providers {
     aws = {
@@ -68,8 +71,8 @@ resource "aws_route_table_association" "public" {
 
 # Security Group
 resource "aws_security_group" "web" {
-  name        = "devops-web-sg"
-  vpc_id      = aws_vpc.main.id
+  name   = "devops-web-sg"
+  vpc_id = aws_vpc.main.id
 
   ingress {
     description = "HTTP"
@@ -84,15 +87,15 @@ resource "aws_security_group" "web" {
     from_port   = 9000
     to_port     = 9000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip] # שימוש במשתנה
   }
 
   ingress {
-    description = "SSH"
+    description = "SSH from Home"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip] # שימוש במשתנה
   }
 
   egress {
@@ -105,21 +108,25 @@ resource "aws_security_group" "web" {
 
 # EC2 Instance
 resource "aws_instance" "web" {
+  key_name = "sprint-feb-2026"
   ami                    = "ami-0014ce3e52359afbd"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web.id]
 
-  user_data = <<-EOF
+user_data = <<-EOF
               #!/bin/bash
               apt-get update
               apt-get install -y docker.io docker-compose
               systemctl start docker
               systemctl enable docker
 
-              # יצירת תיקיית אפליקציה
-              mkdir -p /home/ubuntu/app
+              # יצירת תיקיית אפליקציה ותת-תיקייה ל-HTML
+              mkdir -p /home/ubuntu/app/html
               
+              # יצירת קובץ HTML ראשוני בשרת
+              echo "<h1>Build by Terraform, Managed by Portainer - Omri 2026</h1>" > /home/ubuntu/app/html/index.html
+
               # יצירת קובץ ה-Compose בשרת
               cat <<EOT > /home/ubuntu/app/docker-compose.yml
               version: '3.8'
@@ -128,6 +135,8 @@ resource "aws_instance" "web" {
                   image: nginx:latest
                   ports:
                     - "80:80"
+                  volumes:
+                    - /home/ubuntu/app/html:/usr/share/nginx/html
                   restart: always
                 visualizer:
                   image: portainer/portainer-ce:latest
@@ -146,4 +155,11 @@ resource "aws_instance" "web" {
   tags = {
     Name = "devops-composite-server"
   }
+}
+output "portainer_url" {
+  value = "http://${aws_instance.web.public_ip}:9000"
+}
+
+output "web_server_url" {
+  value = "http://${aws_instance.web.public_ip}"
 }
